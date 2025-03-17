1fadcc4d2376998e42fd68f27a1901b820b556ab
Duvan Betancourt <107191169+DuvanJBetancourt@users.noreply.github.com>
2025-03-16 22:42:14 -0400

    Init Repository

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,15 @@ const config = {
   favicon: 'img/lwms_icon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Fonseca-Advisers-LLC.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/LWMS_DOC',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Fonseca-Advisers-LLC', // Usually your GitHub org/user name.
+  projectName: 'LWMS_DOC', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
